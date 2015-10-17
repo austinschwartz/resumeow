1efd2bc5879645256575fbcf6202acd45bc788a9
nawns <nonis@protonmail.com>
2015-10-17 18:42:01 -0400

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('resumeowApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
